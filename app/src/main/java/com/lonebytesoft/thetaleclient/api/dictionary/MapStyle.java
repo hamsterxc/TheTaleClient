02a2a185bdc6f159a9ee7dd96d5a78ef6716b3dc
Arthur Khamitov <khamitov@wamba.com>
2014-12-18 14:29:26 +0300

    Added new map style: alternative

--- app/src/main/java/com/lonebytesoft/thetaleclient/api/dictionary/MapStyle.java
+++ app/src/main/java/com/lonebytesoft/thetaleclient/api/dictionary/MapStyle.java
@@ -7,6 +7,7 @@ package com.lonebytesoft.thetaleclient.api.dictionary;
 public enum MapStyle {
 
     STANDARD("/game/images/map.png", "Обычная"),
+    ALTERNATIVE("/game/images/map_alternative.png", "Альтернативная"),
     WINTER("/game/images/map_winter.png", "Зимняя"),
     LARGE_PIXEL("/game/images/map_large_pixel.png", "Крупный пиксель"),
     ;
