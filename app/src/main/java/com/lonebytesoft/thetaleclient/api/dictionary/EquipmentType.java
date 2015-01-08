957ff50761041ec1332b4c3f6832dd9f1c15efda
Hamster <artkham@gmail.com>
2015-01-08 23:22:51 +0300

    Fixed equipment order according to website

--- app/src/main/java/com/lonebytesoft/thetaleclient/api/dictionary/EquipmentType.java
+++ app/src/main/java/com/lonebytesoft/thetaleclient/api/dictionary/EquipmentType.java
@@ -11,13 +11,13 @@ public enum EquipmentType {
     MAIN_HAND(0, R.drawable.artifact_main_hand),
     OFF_HAND(1, R.drawable.artifact_off_hand),
     HEAD(2, R.drawable.artifact_head),
+    AMULET(9, R.drawable.artifact_amulet),
     SHOULDERS(3, R.drawable.artifact_shoulders),
     BODY(4, R.drawable.artifact_body),
     GLOVES(5, R.drawable.artifact_gloves),
     CLOAK(6, R.drawable.artifact_cloak),
     TROUSERS(7, R.drawable.artifact_trousers),
     BOOTS(8, R.drawable.artifact_boots),
-    AMULET(9, R.drawable.artifact_amulet),
     RING(10, R.drawable.artifact_ring),
     ;
 
