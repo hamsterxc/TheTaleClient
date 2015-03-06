ae1d39cdaada8d329d1526fc042119fe8b918084
Arthur Khamitov <khamitov@wamba.com>
2015-03-06 14:04:07 +0300

    Added chat messages context menu

--- app/src/main/java/com/lonebytesoft/thetaleclient/fragment/ChatFragment.java
+++ app/src/main/java/com/lonebytesoft/thetaleclient/fragment/ChatFragment.java
@@ -1,7 +1,11 @@
 package com.lonebytesoft.thetaleclient.fragment;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,12 +15,15 @@ import android.text.Spannable;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.StrikethroughSpan;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
-import android.widget.ScrollView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.lonebytesoft.thetaleclient.DataViewMode;
@@ -33,6 +40,7 @@ import com.lonebytesoft.thetaleclient.util.UiUtils;
 import org.xml.sax.XMLReader;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -59,9 +67,9 @@ public class ChatFragment extends WrapperFragment {
     private LayoutInflater layoutInflater;
     private View rootView;
 
-    private ScrollView scroll;
-    private View outerContainer;
-    private ViewGroup container;
+    private ListView chatMessagesList;
+    private ChatAdapter chatAdapter;
+
     private View sendContainer;
     private EditText textMessage;
     private View errorSend;
@@ -72,9 +80,11 @@ public class ChatFragment extends WrapperFragment {
         layoutInflater = inflater;
         rootView = inflater.inflate(R.layout.fragment_chat, container, false);
 
-        scroll = (ScrollView) rootView.findViewById(R.id.chat_scroll);
-        outerContainer = rootView.findViewById(R.id.chat_outer_container);
-        this.container = (ViewGroup) rootView.findViewById(R.id.chat_container);
+        chatMessagesList = (ListView) rootView.findViewById(R.id.chat_list);
+        registerForContextMenu(chatMessagesList);
+        chatAdapter = new ChatAdapter(inflater);
+        chatMessagesList.setAdapter(chatAdapter);
+
         sendContainer = rootView.findViewById(R.id.chat_send_container);
         errorSend = rootView.findViewById(R.id.chat_error_send);
         textMessage = (EditText) rootView.findViewById(R.id.chat_message);
@@ -124,6 +134,41 @@ public class ChatFragment extends WrapperFragment {
         super.onPause();
 
         mainHandler.removeCallbacks(refreshRunnable);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.chat_item, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        final AdapterView.AdapterContextMenuInfo contextMenuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final ChatMessage chatMessage = (ChatMessage) chatAdapter.getItem(contextMenuInfo.position);
+        final String message = Html.fromHtml(chatMessage.message).toString();
+
+        switch(item.getItemId()) {
+            case R.id.chat_action_copy:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    ClipboardManager clipboardManager =
+                            (ClipboardManager) TheTaleClientApplication.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clipData = ClipData.newPlainText("", message);
+                    clipboardManager.setPrimaryClip(clipData);
+                } else {
+                    android.text.ClipboardManager clipboardManager =
+                            (android.text.ClipboardManager) TheTaleClientApplication.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboardManager.setText(message);
+                }
+                return true;
+
+            case R.id.chat_action_quote:
+                setMessageText(String.format("> %s", message));
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
@@ -178,22 +223,16 @@ public class ChatFragment extends WrapperFragment {
                 }
 
                 if(isGlobal) {
-                    container.removeAllViews();
+                    chatAdapter.getMessagesList().clear();
                 }
-                for(final ChatMessage message : messages) {
-                    container.addView(getChatItemView(layoutInflater, container, message));
-                }
-                if(isGlobal || (scroll.getScrollY() == outerContainer.getHeight() - scroll.getBottom())) {
-                    scroll.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                chatAdapter.getMessagesList().addAll(messages);
+                chatAdapter.notifyDataSetChanged();
+
+                if(isGlobal) {
+                    chatMessagesList.post(new Runnable() {
                         @Override
-                        public void onGlobalLayout() {
-                            scroll.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    scroll.fullScroll(View.FOCUS_DOWN);
-                                }
-                            });
-                            UiUtils.removeGlobalLayoutListener(scroll, this);
+                        public void run() {
+                            chatMessagesList.setSelection(chatAdapter.getCount() - 1);
                         }
                     });
                 }
@@ -224,91 +263,145 @@ public class ChatFragment extends WrapperFragment {
         }
     }
 
-    private View getChatItemView(final LayoutInflater layoutInflater, final ViewGroup container, final ChatMessage message) {
-        final View view = layoutInflater.inflate(R.layout.item_chat_message, container, false);
+    private void setMessageText(final String text) {
+        textMessage.setText(text);
+        textMessage.requestFocus();
+        textMessage.setSelection(textMessage.getText().length());
+        UiUtils.showKeyboard(getActivity());
+    }
 
-        final TextView textNickname = (TextView) view.findViewById(R.id.chat_item_nickname);
-        textNickname.setText(message.nickname);
-        textNickname.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String currentMessage = textMessage.getText().toString();
-                textMessage.setText(String.format("%s%s@%s ",
-                        currentMessage,
-                        TextUtils.isEmpty(currentMessage) ? "" : " ",
-                        message.nickname));
+    private class ChatAdapter extends BaseAdapter {
 
-                textMessage.requestFocus();
-                textMessage.setSelection(textMessage.getText().length());
-                UiUtils.showKeyboard(getActivity());
-            }
-        });
+        private final LayoutInflater layoutInflater;
+        private final List<ChatMessage> items;
 
-        final TextView textMessage = (TextView) view.findViewById(R.id.chat_item_message);
-        if(message.isDeleted) {
-            textMessage.setText(message.message);
-            textMessage.setTypeface(textMessage.getTypeface(), Typeface.ITALIC);
-        } else {
-            if(message.message.trim().equals(getString(R.string.chat_message_hr))) {
-                textMessage.setVisibility(View.GONE);
-                view.findViewById(R.id.chat_item_hr).setVisibility(View.VISIBLE);
+        public ChatAdapter(final LayoutInflater layoutInflater) {
+            this.layoutInflater = layoutInflater;
+            this.items = new ArrayList<>();
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            if(convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.item_chat_message, parent, false);
+                holder = new ViewHolder();
+                holder.nickname = (TextView) convertView.findViewById(R.id.chat_item_nickname);
+                holder.message = (TextView) convertView.findViewById(R.id.chat_item_message);
+                holder.hr = convertView.findViewById(R.id.chat_item_hr);
+                holder.time = (TextView) convertView.findViewById(R.id.chat_item_time);
+                convertView.setTag(holder);
             } else {
-                textMessage.setText(Html.fromHtml(message.message,
-                        new Html.ImageGetter() {
-                            @Override
-                            public Drawable getDrawable(String source) {
-                                // not implemented
-                                return null;
-                            }
-                        },
-                        new Html.TagHandler() {
-                            @Override
-                            public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
-                                if(tag.equalsIgnoreCase("del")) {
-                                    final int len = output.length();
-                                    if(opening) {
-                                        output.setSpan(new StrikethroughSpan(), len, len, Spannable.SPAN_MARK_MARK);
-                                    } else {
-                                        final Object span = getSpan(output, StrikethroughSpan.class);
-                                        final int where = output.getSpanStart(span);
-                                        output.removeSpan(span);
-                                        if (where != len) {
-                                            output.setSpan(new StrikethroughSpan(), where, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                                        }
-                                    }
-                                }
-                            }
-
-                            private Object getSpan(final Editable text, final Class<?> kind) {
-                                final Object[] spans = text.getSpans(0, text.length(), kind);
-
-                                if (spans.length == 0) {
-                                    return null;
-                                } else {
-                                    for(int i = spans.length; i > 0; i--) {
-                                        if(text.getSpanFlags(spans[i-1]) == Spannable.SPAN_MARK_MARK) {
-                                            return spans[i-1];
-                                        }
-                                    }
-                                    return null;
-                                }
-                            }
-                        }));
-                textMessage.setMovementMethod(LinkMovementMethod.getInstance());
+                holder = (ViewHolder) convertView.getTag();
             }
+
+            final ChatMessage chatMessage = (ChatMessage) getItem(position);
+
+            holder.nickname.setText(chatMessage.nickname);
+            holder.nickname.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String currentMessage = textMessage.getText().toString();
+                    setMessageText(String.format("%s%s@%s ",
+                            currentMessage,
+                            TextUtils.isEmpty(currentMessage) ? "" : " ",
+                            chatMessage.nickname));
+                }
+            });
+
+            if(chatMessage.isDeleted) {
+                holder.message.setText(chatMessage.message);
+                holder.message.setTypeface(holder.nickname.getTypeface(), Typeface.ITALIC);
+            } else {
+                if(chatMessage.message.trim().equals(getString(R.string.chat_message_hr))) {
+                    holder.message.setVisibility(View.GONE);
+                    holder.hr.setVisibility(View.VISIBLE);
+                } else {
+                    holder.message.setTypeface(holder.nickname.getTypeface(), Typeface.NORMAL);
+                    holder.message.setText(Html.fromHtml(chatMessage.message,
+                            new Html.ImageGetter() {
+                                @Override
+                                public Drawable getDrawable(String source) {
+                                    // not implemented
+                                    return null;
+                                }
+                            },
+                            new Html.TagHandler() {
+                                @Override
+                                public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+                                    if(tag.equalsIgnoreCase("del")) {
+                                        final int len = output.length();
+                                        if(opening) {
+                                            output.setSpan(new StrikethroughSpan(), len, len, Spannable.SPAN_MARK_MARK);
+                                        } else {
+                                            final Object span = getSpan(output, StrikethroughSpan.class);
+                                            final int where = output.getSpanStart(span);
+                                            output.removeSpan(span);
+                                            if (where != len) {
+                                                output.setSpan(new StrikethroughSpan(), where, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                private Object getSpan(final Editable text, final Class<?> kind) {
+                                    final Object[] spans = text.getSpans(0, text.length(), kind);
+
+                                    if (spans.length == 0) {
+                                        return null;
+                                    } else {
+                                        for(int i = spans.length; i > 0; i--) {
+                                            if(text.getSpanFlags(spans[i-1]) == Spannable.SPAN_MARK_MARK) {
+                                                return spans[i-1];
+                                            }
+                                        }
+                                        return null;
+                                    }
+                                }
+                            }));
+                    holder.message.setMovementMethod(LinkMovementMethod.getInstance());
+                }
+            }
+
+            final Calendar calendarCurrent = Calendar.getInstance();
+            final Calendar calendarMessage = Calendar.getInstance();
+            final Date dateMessage = new Date(((long) chatMessage.time) * 1000);
+            calendarMessage.setTime(dateMessage);
+            if((calendarCurrent.get(Calendar.YEAR) == calendarMessage.get(Calendar.YEAR)) && (calendarCurrent.get(Calendar.DAY_OF_YEAR) == calendarMessage.get(Calendar.DAY_OF_YEAR))) {
+                holder.time.setText(dateFormatTime.format(dateMessage));
+            } else {
+                holder.time.setText(dateFormatDate.format(dateMessage));
+            }
+
+            return convertView;
         }
 
-        final Calendar calendarCurrent = Calendar.getInstance();
-        final Calendar calendarMessage = Calendar.getInstance();
-        final Date dateMessage = new Date(((long) message.time) * 1000);
-        calendarMessage.setTime(dateMessage);
-        if((calendarCurrent.get(Calendar.YEAR) == calendarMessage.get(Calendar.YEAR)) && (calendarCurrent.get(Calendar.DAY_OF_YEAR) == calendarMessage.get(Calendar.DAY_OF_YEAR))) {
-            ((TextView) view.findViewById(R.id.chat_item_time)).setText(dateFormatTime.format(dateMessage));
-        } else {
-            ((TextView) view.findViewById(R.id.chat_item_time)).setText(dateFormatDate.format(dateMessage));
+        public List<ChatMessage> getMessagesList() {
+            return items;
         }
 
-        return view;
+        private class ViewHolder {
+            public TextView nickname;
+            public TextView message;
+            public View hr;
+            public TextView time;
+        }
+
     }
 
 }
