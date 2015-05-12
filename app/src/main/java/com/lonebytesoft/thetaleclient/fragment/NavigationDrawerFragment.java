7f09e533a376f41a81616c82923d02b8578c6346
Hamster <artkham@gmail.com>
2015-05-12 14:28:50 +0300

    Fixed navigation drawer

--- app/src/main/java/com/lonebytesoft/thetaleclient/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/lonebytesoft/thetaleclient/fragment/NavigationDrawerFragment.java
@@ -3,12 +3,12 @@ package com.lonebytesoft.thetaleclient.fragment;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -98,7 +98,6 @@ public class NavigationDrawerFragment extends Fragment {
         mDrawerToggle = new ActionBarDrawerToggle(
                 getActivity(),               /* host Activity */
                 mDrawerLayout,               /* DrawerLayout object */
-                R.drawable.ic_drawer,        /* nav drawer image to replace 'Up' caret */
                 R.string.drawer_title_open,  /* "open drawer" description for accessibility */
                 R.string.drawer_title_close  /* "close drawer" description for accessibility */
         ) {
